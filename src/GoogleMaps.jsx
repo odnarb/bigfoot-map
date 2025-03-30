9a14151bee49728c10268a0dd487694ec469eb37
Brandon Chambers <bran.cham@gmail.com>
2025-03-29 19:09:04 -0600

    Use env vars with vite. Removed unused imports.

--- src/GoogleMaps.jsx
+++ src/GoogleMaps.jsx
@@ -14,7 +14,7 @@ const center = {
 function GoogleMaps() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
   })
 
   const [map, setMap] = React.useState(null)
