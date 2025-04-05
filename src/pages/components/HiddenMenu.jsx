fd61a34de99fdcbaac873cc128098655c3f0200c
Brandon Chambers <bran.cham@gmail.com>
2025-04-05 14:06:09 -0600

    Added hidden menu

---
+++ src/pages/components/HiddenMenu.jsx
@@ -0,0 +1,173 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+
+import {
+    Box, Toolbar,
+    AppBar as MuiAppBar,
+    List, CssBaseline, Typography, Divider, IconButton,
+    ListItem, ListItemButton, ListItemText, ListItemIcon,
+    Drawer
+} from '@mui/material';
+
+import {
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon,
+    Menu as MenuIcon,
+    Report as ReportIcon,
+    VolunteerActivism as VolunteerActivismIcon,
+    Home as HomeIcon,
+    HelpCenter as HelpCenterIcon
+} from '@mui/icons-material';
+
+import { Link, Outlet } from 'react-router';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+          marginLeft: 0,
+        },
+      },
+    ],
+  }),
+);
+
+const menuItems = [
+    { text: 'Home', url: '/', icon: <HomeIcon /> },
+    { text: 'Submit Report', url: '/submit-report', icon: <ReportIcon /> },
+    { text: 'Donate', url: '/donate', icon: <VolunteerActivismIcon /> },
+    { text: 'About', url: '/about', icon: <HelpCenterIcon /> }
+]
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function HiddenMenu() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={[
+              {
+                mr: 2,
+              },
+              open && { display: 'none' },
+            ]}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">Mapping Bigfoot</Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+        {menuItems.map((item, index) => (
+            <ListItem component={Link} key={index} disablePadding sx={{ display: 'block' }} to={item.url}>
+              <ListItemButton sx={[{minHeight: 48, px: 2.5}, open ? {justifyContent: 'initial'} : {justifyContent: 'center'}]}>
+                <ListItemIcon sx={[{minWidth: 0, justifyContent: 'center'}, open ? {mr: 3} : { mr: 'auto'}]}>
+                    {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.text} sx={[ open? {opacity: 1} : { opacity: 0} ]} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+          
+          {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))} */}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+ 
+        <Outlet />
+
+      </Main>
+    </Box>
+  );
+}
