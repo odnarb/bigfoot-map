6bf47d2c99a8a87cd38435177022c936d97c510e
Brandon Chambers <bran.cham@gmail.com>
2026-02-07 14:25:36 -0700

    Update report page

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,8 +1,8 @@
-export default function Report () {
+export default function Report() {
     return (
         <>
             <h1>Submit A Report</h1>
-            <p>Enter as much information as you can. You can come back later and update the report.</p>
+            <p>If this project gets traction we could have it hosted and submit reports.</p>
         </>
     )
 }
